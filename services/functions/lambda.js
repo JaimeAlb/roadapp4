a2efaf94fb8c2b27df05f85605449190015c9aa6
Jaime Albornoz <albornozsotojaime@gmail.com>
2022-09-24 16:56:35 -0300

    dynamoDB table created - notes

--- services/functions/lambda.js
+++
@@ -1,7 +0,0 @@
-export const handler1 = async (event) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Helloooo, World! Your request was received at ${event.requestContext.time}.`,
-  };
-};
