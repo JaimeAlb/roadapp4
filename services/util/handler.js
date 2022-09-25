15859b0c688f0bba7667259605d0389719b4311d
Jaime Albornoz <albornozsotojaime@gmail.com>
2022-09-25 14:02:36 -0300

    refactor code to handle Lambda functions

---
+++ services/util/handler.js
@@ -0,0 +1,23 @@
+export default function handler(lambda) {
+
+  // We are creating a handler function that we’ll use as a wrapper around our Lambda functions.
+  return async function (event, context) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e) {
+      console.error(e);
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
