0ea97a78f64aa627029fde62b1e171b4192e9260
Jaime Albornoz <albornozsotojaime@gmail.com>
2022-09-26 14:30:40 -0300

    enabling cors

--- services/util/handler.js
+++ services/util/handler.js
@@ -15,9 +15,18 @@ export default function handler(lambda) {
     }
 
     // Return HTTP response
+    // return {
+    //   statusCode,
+    //   body: JSON.stringify(body),
+    // };
+    // cors headers
     return {
       statusCode,
       body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
     };
   };
 }
--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -5,7 +5,19 @@ import { Bucket, Table } from "@serverless-stack/resources";
 export function StorageStack({ stack, app }) {
   
   // Create an S3 bucket
-  const bucket = new Bucket(stack, "Uploads");
+  // const bucket = new Bucket(stack, "Uploads");
+
+  // To Handle CORS in S3 for File Uploads
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
 
 
   // Create the DynamoDB table
